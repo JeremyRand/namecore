413dd2617551b2d1472cb90ab78cd88e0e4073e5
Daniel Kraft <d@domob.eu>
2014-11-14 22:24:38 +0100

    Add unit tests for auxpow verification.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -329,6 +329,7 @@ public:
     virtual void setDefaultCheckMemPool(bool afDefaultCheckMemPool)  { fDefaultCheckMemPool=afDefaultCheckMemPool; }
     virtual void setAllowMinDifficultyBlocks(bool afAllowMinDifficultyBlocks) {  fAllowMinDifficultyBlocks=afAllowMinDifficultyBlocks; }
     virtual void setSkipProofOfWorkCheck(bool afSkipProofOfWorkCheck) { fSkipProofOfWorkCheck = afSkipProofOfWorkCheck; }
+    virtual void setProofOfWorkLimit(const uint256& limit) { bnProofOfWorkLimit = limit; }
 };
 static CUnitTestParams unitTestParams;
 
